926d3bb5dd6955f680cfce312ce6daffb74b853b
Andy Tang <tang.andy91@gmail.com>
2016-04-05 22:36:21 -0500

    Created Color Picker.

---
+++ Cheers/Cheers/colorPicker.swift
@@ -0,0 +1,13 @@
+//
+//  colorPicker.swift
+//  Cheers
+//
+//  Created by Andy Tang on 4/5/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+import UIKit
+
+class colorPicker: UIViewController {
+
+}
